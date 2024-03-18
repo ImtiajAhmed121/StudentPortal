c1cbc7cd7eed47282f0e535146f35b19115ae0d3
ImtiajAhmed121 <ahmedsabbir3010@gmail.com>
2024-03-19 05:21:51 +0600

    Add project files.

---
+++ Models/Student.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace StudentPortal.Models
+{
+    public partial class Student
+    {
+        public Guid Id { get; set; }
+        public string? Name { get; set; }
+        public int? Gender { get; set; }
+        public DateTime? Dob { get; set; }
+        public int? ClassId { get; set; }
+        public DateTime? CreatedDate { get; set; }
+        public DateTime? ModificationDate { get; set; }
+
+        public virtual Class? Class { get; set; }
+    }
+}
---
+++ Models/StudentDBContext.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace StudentPortal.Models
+{
+    public partial class StudentDBContext : DbContext
+    {
+        public StudentDBContext()
+        {
+        }
+
+        public StudentDBContext(DbContextOptions<StudentDBContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Class> Classes { get; set; } = null!;
+        public virtual DbSet<Student> Students { get; set; } = null!;
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+                optionsBuilder.UseSqlServer("Server=DESKTOP-KQGG6BD\\SQLEXPRESS;Database=StudentDB;Trusted_Connection=True;Encrypt=False;MultipleActiveResultSets=true;");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Class>(entity =>
+            {
+                entity.ToTable("Class");
+
+                entity.Property(e => e.CreatedDate).HasColumnType("datetime");
+
+                entity.Property(e => e.ModificationDate).HasColumnType("datetime");
+
+                entity.Property(e => e.Name)
+                    .HasMaxLength(50)
+                    .IsUnicode(false);
+            });
+
+            modelBuilder.Entity<Student>(entity =>
+            {
+                entity.ToTable("Student");
+
+                entity.Property(e => e.Id).ValueGeneratedNever();
+
+                entity.Property(e => e.CreatedDate).HasColumnType("datetime");
+
+                entity.Property(e => e.Dob)
+                    .HasColumnType("datetime")
+                    .HasColumnName("DOB");
+
+                entity.Property(e => e.ModificationDate).HasColumnType("datetime");
+
+                entity.Property(e => e.Name)
+                    .HasMaxLength(50)
+                    .IsUnicode(false);
+
+                entity.HasOne(d => d.Class)
+                    .WithMany(p => p.Students)
+                    .HasForeignKey(d => d.ClassId)
+                    .HasConstraintName("FK__Student__ClassId__398D8EEE");
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
